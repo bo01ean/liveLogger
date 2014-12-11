01214700ba6f215291f052dba2ec1767a18e2aec
Nathan Trujillo <nathan@drivecurrent.com>
2014-12-11 01:37:18 -0800

    Test that the socket works Serve Frontend from node Join backend and front end in node process Make socket.io an Angular Service create package.json and npm-shrinkwrap.json Unicorns

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,71 +1,86 @@
-var liveLoggerApp = angular.module('liveLoggerApp', ['ngGrid','ui.bootstrap']);
-
 var ModalInstanceCtrl = function ($scope, $modalInstance, content) {
-
     $scope.content = content;
-    $scope.selected = {
-
-    };
-
+    $scope.selected = {};
     $scope.ok = function () {
         $modalInstance.close();
     };
-
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
     };
 }
 
-liveLoggerApp.controller('LiveLogCtrl', function ($scope, $http, $modal, $log) {
-
-    $scope.logs = [];
-    $scope.filterOptions = {        filterText: ''    };
-    $scope.detail = '<span ng-click="openModal(row.entity[col.field],\'lg\')"><pre>{{row.entity[col.field]}}</pre></span>';
-
-    $scope.openModal = function (data, size) {
-        var modalInstance = $modal.open({
-            templateUrl: 'myModalContent.html',
-            controller: ModalInstanceCtrl,
-            size: size,
-            resolve: {
-                content: function () {
-                    return {header:"Detailed Log Output", body:data}
-                }
+var socket = io.connect("http://localhost:8088");
+var app = angular.module('liveLoggerApp', ['ngGrid','ui.bootstrap'])
+    .service('socket', function ($rootScope) {
+        return {
+            on: function (eventName, callback) {
+                console.log("on");
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function (eventName, data, callback) {
+                console.log("emit");
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
+                })
             }
+        };
+    })
+    .controller('LiveLogCtrl', function ($scope, $modal) {
+        $scope.logs = [];
+        $scope.filterOptions = {filterText:''};
+        $scope.detail = '<span ng-click="openModal(row.entity[col.field],\'lg\')"><pre>{{ row.entity[col.field] }}</pre></span>';
+
+        $scope.openModal = function (data, size) {
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: ModalInstanceCtrl,
+                size: size,
+                resolve: {
+                    content: function () {
+                        return {header:"Detailed Log Output", body:data}
+                    }
+                }
+            });
+            modalInstance.result.then(
+                function () { },
+                function () { $log.info('Modal dismissed at: ' + new Date()); }
+            );
+        };
+
+        $scope.gridOptions = {
+            data:'logs',
+            showGroupPanel: true,
+            filterOptions: $scope.filterOptions,
+            columnDefs: [
+                {field: 'host', displayName: 'Host', width: "10%"},
+                {field: 'log', displayName:'Log', cellTemplate: $scope.detail, width:"80%"},
+                {field: 'caller', displayName: 'Caller', cellTemplate: $scope.detail, width: "10%"}
+        ]};
+
+        socket.on('connected',function(data){
+            $scope.$apply(function(){});
         });
-        modalInstance.result.then(function () {
 
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
+        socket.on('action',function(data){
+            $scope.$apply(function(){});
         });
-    };
 
-    $scope.gridOptions = {
-        data:'logs',
-        showGroupPanel: true,
-        filterOptions: $scope.filterOptions,
-        columnDefs: [
-            { field: 'host', displayName: 'Host', width: "10%" },
-            { field: 'log', displayName:'Log', cellTemplate: $scope.detail, width:"80%"},
-            { field: 'caller', displayName: 'Caller', cellTemplate: $scope.detail, width: "10%" }
-        ]
-    };
+        socket.on('log',function(data){
+            $scope.$apply(function(){$scope.logs.unshift(data);});
+        });
 
-    var socket = io.connect("http://localhost:8888");
+        $scope.test = function(){
+            socket.emit('test', {host:"localhost", log: "Testing Front End", caller:"Unknown"});
+        }
 
-    socket.on('connected',function(data){
-        $scope.$apply(function(){});
-        console.log("connected!");
     });
-
-    socket.on('action',function(data){
-        $scope.$apply(function(){});
-        console.log(data);
-    });
-
-    socket.on('log',function(data){
-        $scope.$apply(function(){$scope.logs.unshift(data);});
-        console.log(data);
-    });
-
-});
--- server.js
+++ server.js
@@ -1,39 +1,47 @@
-// to run issue:  npm install -g connect serve-static socket.io http
-var app = require('http').createServer(handler),
-io = require('socket.io').listen(app, {log:false, origins:'*:*'});
+    // liveLogger server
+    // npm install -g connect serve-static socket.io http protractor
+    // npm uninstall -g connect serve-static socket.io http protractor
+    var app = require('http').createServer(handler);
+    var io = require('socket.io').listen(app, {log:false, origins:'*:*'});
+
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
+
+    // Front End Runs Here
+    // Load via http://localhost:8880
+    connect().use(serveStatic(__dirname)).listen(8880);
+    // Logger Listens Here on this address
+    app.listen(8088);
+
+    function handler (req, res) {
+        res.writeHead(200, {'Access-Control-Allow-Origin' : '*'});
+        res.end();
+    }
+
+    io.sockets.on('connection', function (socket) {
+
+        console.log('FrontEnd connection from: ' + socket.handshake.headers.host);
+
+        socket.on('action', function (data) {
+            console.log(socket.handshake.headers.host, 'action:' + data);
+            socket.emit('action', data);
+        });
+
+        socket.on('command', function (data) {
+            console.log(socket.handshake.headers.host, 'command:' + data);
+            socket.emit('command', data);
+        });
+
+        socket.on('test', function (data) {
+            console.log(socket.handshake.headers.host, 'test:' + data);
+            socket.emit('log', {host:"localhost", log: "Response From Backend", caller: "AngularJS"});
+        });
+
+        socket.on('log', function (data) {
+            console.log(socket.handshake.headers.host, 'log:' + data);
+            //socket.emit('log', {host:"localhost", log: "Testing Front End", caller: "AngularJS"});
+        });
 
 
-var connect = require('connect');
-var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(8880);
-app.listen(8088);
-
-function handler (req, res) {
-  res.writeHead(200, {'Access-Control-Allow-Origin' : '*'});
-  res.end();
-}
-
-io.sockets.on('connection', function (socket) {
-  var addr = socket.handshake.address;
-  socket.emit('connected');
-  console.log('connection from: ' + addr.address);
-
-  socket.on('action', function (data) {
-    console.log('action:' + data);
-    socket.broadcast.emit('action', data);
-  });
-
-  socket.on('command', function (data) {
-    console.log('command:' + data);
-    socket.broadcast.emit('command', data);
-  });
-
-
-  socket.on('log', function (data) {
-    console.log('log:' + data);
-    socket.broadcast.emit('log', data);
-  });
-
-
-});
+    });
